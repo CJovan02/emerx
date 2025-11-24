4bcc85a02aa406faba2127622b81d0e31c551508
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2025-11-24 20:01:40 +0100

    Added items normalization (handling duplicates)

--- backend/src/Services/Orders/OrderService.cs
+++ backend/src/Services/Orders/OrderService.cs
@@ -54,13 +54,24 @@ public class OrderService(
                 return Result<OrderResponse>.Failure(UserErrors.NotFound(userId));
             }
 
+            // We normalize ordered items, we sum up the quantities of items that have the same id
+            // If some bug on frontend happens and duplicate items get sent
+            var normalizedItems = request.Items
+                .GroupBy(x => x.ProductId)
+                .Select(g => new OrderItemRequest(
+                    ProductId: g.Key,
+                    Quantity: g.Sum(x => x.Quantity))
+                ).ToList();
+            var normalizedRequest = request with { Items = normalizedItems };
+
             // We need to fetch all the products ordered, to see if all the requested ids are correct and
             // to check the price and the name of the products.
             // We could send the price and the name from the frontend inside a request to save on the database reads but
             // that is a security hole, price needs to be read from the database and not specified.
 
-            var productIds = request.Items.Select(x => ObjectId.Parse(x.ProductId)).ToList();
+            var productIds = normalizedItems.Select(x => ObjectId.Parse(x.ProductId)).ToList();
             var products = (await productRepository.GetProductsByIds(productIds, session)).ToList();
+
             var missingProducts = productIds.Except(products.Select(x => x.Id)).ToList();
             if (missingProducts.Any())
             {
@@ -68,7 +79,7 @@ public class OrderService(
                 return Result<OrderResponse>.Failure(OrderErrors.NotFound(missingProducts));
             }
 
-            var order = request.ToDomain(products);
+            var order = normalizedRequest.ToDomain(products);
             await orderRepository.CreateOrder(order, session);
 
             await session.CommitTransactionAsync();
