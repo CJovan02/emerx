bb59664e5a63479bb879b8c9a0e14d261bf507cc
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2026-05-28 10:45:03 +0200

    api-testing claims

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using EMerx.Auth;
 using EMerx.DTOs.Id;
 using EMerx.Infrastructure.CloudinaryContext;
 using EMerx.Infrastructure.MongoDb;
@@ -57,10 +59,33 @@ if (!app.Environment.IsEnvironment("Api-Testing"))
 app.UseAuthentication();
 app.UseAuthorization();
 
+// dotnet run --launch-profile Api-Testing
+// Used during api-testing. Since some endpoints require valid JWT in order to work and
+// there is no way to acquire JWT using the Firebase SDK (auth service this server uses), for api testing purpuses
+// we need to inject fake claims in order to test the API.
+// This is not required during end-to-end tests, since we easily log on the frontend.
 if (app.Environment.IsEnvironment("Api-Testing"))
 {
     Console.WriteLine("Api-Testing environment is running.\n" +
                       "Injecting fake auth claims into context...");
+
+    app.Use(async (context, next) =>
+    {
+        if (context.Request.Headers.ContainsKey("Test-Auth"))
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, ((int)Roles.Admin).ToString()),
+                new Claim(ClaimTypes.NameIdentifier, "test-user"),
+                new Claim(ClaimTypes.Email, "testuser@test.com")
+            };
+
+            var identity = new ClaimsIdentity(claims, "Api-Test");
+            context.User = new ClaimsPrincipal(identity);
+        }
+
+        await next();
+    });
 }
 
 app.Run();
